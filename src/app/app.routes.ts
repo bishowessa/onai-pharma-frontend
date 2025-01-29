c1d4441ee6ef0bbc781f7a47f17d0b4852553a56
bishowessa <bishowessa@gmail.com>
2025-01-29 19:35:09 +0200

    navbar done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './components/home/home.component';
+// import { ProductListComponent } from './components/product/product-list/product-list.component';
+// import { OrderComponent } from './components/order/order.component';
+// import { AdminComponent } from './components/admin/admin.component';
+// import { AuthGuard } from './guards/auth.guard';
+// import { AdminGuard } from './guards/admin.guard';
+
+// const routes: Routes = [
+//   { path: '', component: HomeComponent },
+//   { path: 'products', component: ProductListComponent },
+//   { path: 'order', component: OrderComponent, canActivate: [AuthGuard] },
+//   { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] },
+//   { path: '**', redirectTo: '' },
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
