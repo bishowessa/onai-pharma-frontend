3dd6773549cc7387d81ee6da88d323490ae1fbda
learnaholic-bit <george.m.wessa+github@gmail.com>
2025-10-29 20:00:56 +0300

    Add missing testing modules to component specs
    Added HttpClientTestingModule and RouterTestingModule to various component and service spec files to ensure proper test isolation and support for HTTP and routing dependencies. This improves test reliability and prevents errors related to missing providers during unit testing.

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -10,7 +10,7 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent, HttpClientTestingModule]
+      imports: [RegisterComponent, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
